f334bbf8789914a33ba12f9d83119e7910fd1fa6
Vinayak Chaurasia <vinayakchaurasia@rathi.com>
2023-07-23 21:50:39 +0530

    Initial commit

---
+++ src/test/java/com/vinayak/kafkaproducerdemo/KafkaProducerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinayak.kafkaproducerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
