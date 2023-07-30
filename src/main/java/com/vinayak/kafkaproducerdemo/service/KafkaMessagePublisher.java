0133cdfe8398db4a4adb49e3c6484832a24819d5
Vinayak Chaurasia <vinayakchaurasia@rathi.com>
2023-07-30 20:41:49 +0530

    Added publisher service

---
+++ src/main/java/com/vinayak/kafkaproducerdemo/service/KafkaMessagePublisher.java
@@ -0,0 +1,28 @@
+package com.vinayak.kafkaproducerdemo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class KafkaMessagePublisher {
+
+    @Autowired
+    private KafkaTemplate<String, Object> template;
+
+    public void sendMessageToTopic(String message) {
+        CompletableFuture<SendResult<String, Object>> future = template.send("kafka-auto-topic", message);
+        future.whenComplete((result, exception) -> {
+            if (exception == null) {
+                System.out.println("Sent message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            }
+            else {
+                System.out.println("Unable to send message=[" + message + "] due to =[" + exception.getMessage() + "]");
+            }
+        });
+
+    }
+}
